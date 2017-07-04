9f6563f8c7aae16fd9c31f76127e9279ede6f9a3
Dexter Bengil <dexterb2992@gmail.com>
2017-07-04 21:46:56 +0800

    fix jpegoptim

--- lib/traits/JpegCompressor.php
+++ lib/traits/JpegCompressor.php
@@ -15,13 +15,13 @@ trait JpegCompressor{
 	}
 
 	public function jpegOptim($source_path, $destination_dir){
-		$command = "jpegoptim --strip-all ".escapeshellarg($source_path)." -d ".escapeshellarg($destination_dir);
+		$command = "jpegoptim --strip-all -q ".escapeshellarg($source_path)." -d ".escapeshellarg($destination_dir);
 
 		$output = null;
 		
 		$response = system($command, $output);
 
-		if( file_exists($destination_url) )
+		if( file_exists($destination_dir.basename($source_path)) )
 			return $destination_url;
 		return false;
 	}

742a4946c83ce36f4c8a60b1e09c7f93183b2354
Dexter Bengil <dexterb2992@gmail.com>
2017-07-04 16:07:05 +0800

    test on 2 jpeg compressors

--- lib/traits/JpegCompressor.php
+++ lib/traits/JpegCompressor.php
@@ -4,12 +4,25 @@ namespace App\lib\traits;
 trait JpegCompressor{
 
 	public function optimizeJpeg($source_path, $destination_url){
-		$jpegtran = $this->jpegTran($source_path, $destination_url);
-		return $jpegtran;
+		$source_path = escapeshellarg($source_path);
+		$destination_url = escapeshellarg($destination_url);
+
+		$jpegTran = $this->jpegTran($source_path, $destination_url);
+		$jpegOptim = $this->jpegOptim($source_path, $destination_url);
+
+		$size1 = $jpegTran != false ?  $size1 = @filesize($jpegTran) : 0;
+		$size2 = $jpegOptim != false ? $size2 = @filesize($jpegOptim) : 0;
+
+		if( $size1 > $size2 )
+			return $jpegOptim;
+		else
+			return $jpegTran;
+
+		return false;
 	}
 
 	private function jpegTran($source_path, $destination_url){
-		$command = "jpegtran -copy none -progressive -optimize ".escapeshellarg($source_path)." > ".escapeshellarg($destination_url);
+		$command = "jpegtran -copy none -progressive -optimize $source_path > $destination_url";
 		$output = null;
 		
 		$response = system($command, $output);
@@ -20,6 +33,14 @@ trait JpegCompressor{
 	}
 
 	private function jpegOptim($source_path, $destination_url){
+		$command = "jpegoptim --strip-all --all-progressive $source_path --dest=$destination_url";
 
+		$output = null;
+		
+		$response = system($command, $output);
+
+		if( file_exists($destination_url) )
+			return $destination_url;
+		return false;
 	}
 }

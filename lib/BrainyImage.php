ca2d46aff3e896e393716413b1b092ae18270348
Dexter Bengil <dexterb2992@gmail.com>
2017-09-22 19:13:02 +0800

    enabled error_reporting

--- lib/BrainyImage.php
+++ lib/BrainyImage.php
@@ -4,125 +4,123 @@ namespace App\lib;
 use App\lib\traits\ImageCompressor;
 use App\lib\Helper;
 
-class BrainyImage {
-	use ImageCompressor;
+class BrainyImage
+{
+    use ImageCompressor;
 
-	function __construct($dir = 'uploads/source/', $extensions = array("image/jpeg","image/png"))
-	{
-		$this->error = '';
-		$this->img = '';
-		$this->dir = $dir;
-		$this->extensions = $extensions;
+    public function __construct($dir = 'uploads/source/', $extensions = array("image/jpeg","image/png"))
+    {
+        $this->error = '';
+        $this->img = '';
+        $this->dir = $dir;
+        $this->extensions = $extensions;
 
-		$this->filename = "";
-		$this->result = array();
-	}
+        $this->filename = "";
+        $this->result = array();
+    }
 
-	public function upload($file){
-		foreach($file['img_file']['tmp_name'] as $key => $tmp_name ){
-			$filename = preg_replace( '/[^a-z0-9.]+/', '_', strtolower( $file['img_file']['name'][$key] ) );
+    public function upload($file)
+    {
+        foreach ($file['img_file']['tmp_name'] as $key => $tmp_name) {
+            $filename = preg_replace('/[^a-z0-9.]+/', '_', strtolower($file['img_file']['name'][$key]));
 
-			$file_size =$file['img_file']['size'][$key];
-			$file_tmp =$file['img_file']['tmp_name'][$key];
-			$file_type=$file['img_file']['type'][$key];
+            $file_size =$file['img_file']['size'][$key];
+            $file_tmp =$file['img_file']['tmp_name'][$key];
+            $file_type=$file['img_file']['type'][$key];
 
-			$var = explode('.',$filename);
-			$file_ext = strtolower($var[1]);
+            $var = explode('.', $filename);
+            $file_ext = strtolower($var[1]);
 
-			$target = $this->dir.$filename;
+            $target = $this->dir.$filename;
 
-			$results = array();
+            $results = array();
 
+            if (in_array($file_type, $this->extensions) === true) {
+                if (move_uploaded_file($file_tmp, $this->dir.$filename)) {
+                    $results = array(
+                        "success" => 1,
+                        "url" => $this->dir.$filename,
+                        "filename" => $filename
+                    );
+                } else {
+                    $this->error = 'Error in uploading few files. Some files couldn\'t be uploaded.';
+                }
+            } else {
+                $this->error = 'Error in uploading few files. File type is not allowed.';
+            }
+            $results['info'] = $tmp_name;
+            $results['filetype'] = $file_type;
+        }
 
-			if(in_array($file_type,$this->extensions ) === true)
-			{
-				if(move_uploaded_file($file_tmp, $this->dir.$filename))
-				{
-					
-					$results = array(
-						"success" => 1, 
-						"url" => $this->dir.$filename,
-						"filename" => $filename
-					);
-				}else
-					$this->error = 'Error in uploading few files. Some files couldn\'t be uploaded.';				
-			}else{
-				$this->error = 'Error in uploading few files. File type is not allowed.';
-			}
-			$results['info'] = $tmp_name;
-			$results['filetype'] = $file_type;
-		}
+        if ($this->error != "") {
+            $results["error"] = $this->error;
+            $results['success'] = 0;
+        }
+        return $results;
+    }
 
-		if( $this->error != "" ){
-			$results["error"] = $this->error;
-			$results['success'] = 0;
-			
-		}
-		
+    public function compressUploaded($source_photo, $filename)
+    {
+        $uniqueId = uniqid(rand(), true);
+        mkdir('uploads/output/'.$uniqueId);
+        $dest_dir = 'uploads/output/'.$uniqueId.'/';
+         
+        $info = getimagesize($source_photo);
 
-		return $results;	
-	}
+        if ($info['mime'] == 'image/jpeg') {
+            $output_file = $this->compressJPEG($source_photo, $dest_dir);
 
-	public function compressUploaded($source_photo, $filename){
-		$uniqueId = uniqid(rand(), true);
-		mkdir('uploads/output/'.$uniqueId);
-		$dest_dir = 'uploads/output/'.$uniqueId.'/';
-		 
-		$info = getimagesize($source_photo);
+            if ($output_file != false) {
+                return $this->returnSizeDifference($source_photo, $output_file, $info);
+            } else {
+                return json_encode(
+                    array(
+                        "error" => "Something went wrong while trying to compress a JPEG file.",
+                        "success" => 0
+                    )
+                );
+            }
+        } elseif ($info['mime'] == 'image/png') {
+            $dest_path = $this->compressPNG($source_photo, $dest_dir);
+            
+            if (is_array($dest_path)) {
+                return $dest_path;
+            } else {
+                return $this->returnSizeDifference($source_photo, $dest_path, $info);
+            }
+        }
+    }
 
-		if ($info['mime'] == 'image/jpeg'){
-			$output_file = $this->compressJPEG($source_photo, $dest_dir);
+    public function returnSizeDifference($source_photo, $dest_photo, $info)
+    {
+        $old_size = @filesize($source_photo);
+        $new_size = @filesize($dest_photo);
+        $size_difference = $old_size - $new_size;
 
-			if( $output_file != false ){
-				return $this->returnSizeDifference($source_photo, $output_file, $info);
-			}else{
-				return json_encode(array(
-					"error" => "Something went wrong while trying to compress a JPEG file.", 
-					"success" => 0)
-				);
-			}
+        $percentage_saved = (($size_difference)/$old_size)*100;
 
-		}else if($info['mime'] == 'image/png'){
+        if ($new_size > $old_size) {
+            $new_size = $old_size;
+            $dest_photo = $source_photo;
+            $percentage_saved = 0;
+        }
 
-			$dest_path = $this->compressPNG($source_photo, $dest_dir);
-			
-			if( is_array($dest_path) ){
-				return $dest_path;
-			}else{
-				return $this->returnSizeDifference($source_photo, $dest_path, $info);
-			}
-		}
-	}
+        $results = array(
+            "success" => 1,
+            "input" => array(
+                "size" => Helper::formatSizeUnits($old_size),
+                "filename" => $this->filename,
+                "type" => $info['mime'],
+                "url" => $source_photo
+           ),
+            "output" => array(
+                "size" => Helper::formatSizeUnits($new_size),
+                "type" => $info['mime'],
+                "url" => $dest_photo,
+                "diff" => round($percentage_saved, 2).'%'
+           )
+        );
 
-	public function returnSizeDifference($source_photo, $dest_photo, $info){
-		$old_size = @filesize($source_photo);
-		$new_size = @filesize($dest_photo);
-		$size_difference = $old_size - $new_size;
-
-		$percentage_saved = (($size_difference)/$old_size)*100;
-
-		if( $new_size > $old_size ){
-			$new_size = $old_size;
-			$dest_photo = $source_photo;
-			$percentage_saved = 0;
-		}
-
-		$results = array(
-			"success" => 1,
-			"input" => array(
-				"size" => Helper::formatSizeUnits($old_size),
-				"filename" => $this->filename,
-				"type" => $info['mime'],
-				"url" => $source_photo
-			),
-			"output" => array(
-				"size" => Helper::formatSizeUnits($new_size),
-				"type" => $info['mime'],
-				"url" => $dest_photo,
-				"diff" => round($percentage_saved, 2).'%'
-			)
-		);
-
-		return $results;
-	}
+        return $results;
+    }
 }
--- process.php
+++ process.php
@@ -1,69 +1,75 @@
 <?php
 namespace App;
 
+error_reporting(1);
+
 require_once "conf.php";
 
 use \App\lib\BrainyImage;
 
-class AjaxProcessImage{
+class AjaxProcessImage
+{
+    public function __construct()
+    {
+        $this->brainyImage = new BrainyImage();
 
-	function __construct()
-	{
-		$this->brainyImage = new BrainyImage();
+        if (isset($_POST['q'])) {
+            foreach ($_POST as $key => $value) {
+                $_POST[$key] = trim($_POST[$key]);
+            }
+            switch ($_POST['q']) {
+                case 'upload':
+                    $this->upload($_FILES);
+                    break;
+                
+                case 'compress':
+                    $filename = $_POST['filename'];
+                    if ($filename == '**from_url**') {
+                        $filename = basename($_POST['src']);
+                    }
 
-		if( isset($_POST['q']) ){
-			foreach ($_POST as $key => $value) {
-				$_POST[$key] = trim($_POST[$key]);
-			}
-			switch ($_POST['q']) {
-				case 'upload':
-					$this->upload($_FILES);
-					break;
-				
-				case 'compress':
-					$filename = $_POST['filename'];
-					if( $filename == '**from_url**' ){
-						$filename = basename($_POST['src']);
-					}
+                    $this->compress($_POST['src'], $filename);
+                    break;
 
-					$this->compress($_POST['src'], $filename);
-					break;
+                case 'retry':
+                    $this->retry($_POST['src'], basename($_POST['src']));
+                    break;
 
-				case 'retry':
-					$this->retry( $_POST['src'], basename($_POST['src']) );
-					break;
+                case 'register':
+                    $this->register($_POST);
+                    break;
+            }
+        }
+    }
 
-				case 'register':
-					$this->register($_POST);
-					break;
-			}
-		}
-	}
+    private function upload($files)
+    {
+        if (isset($files['img_file'])) {
+            $response = $this->brainyImage->upload($files);
+        } else {
+            $response = array("error" => true, "msg" => "No image received.");
+        }
 
-	private function upload($files){
-		if( isset($files['img_file']) ){
-			$response = $this->brainyImage->upload($files);
-		}else{
-			$response = array("error" => true, "msg" => "No image received.");
-		}
+        echo json_encode($response);
+    }
 
-		echo json_encode($response);
-	}
+    private function compress($src, $filename = "")
+    {
+        $res = $this->brainyImage->compressUploaded($src, $filename);
 
-	private function compress($src, $filename = ""){
-		$res = $this->brainyImage->compressUploaded($src, $filename);
+        echo is_array($res) ? json_encode($res) : $res;
+    }
 
-		echo is_array($res) ? json_encode($res) : $res;
-	}
+    private function retry($src, $filename = "")
+    {
+        return $this->compress($src, $filename);
+    }
 
-	private function retry($src, $filename = ""){
-		return $this->compress($src, $filename);
-	}
-
-	// POST DATA
-	private function register($input){
-		
-	}
+    // POST DATA
+    private function register($input)
+    {
+        //
+    }
 }
 
 new AjaxProcessImage();

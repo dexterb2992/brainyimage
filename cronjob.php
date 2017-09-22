d6a8e0b4b2c7153b53401b9642932a4965793b7c
Dexter Bengil <dexterb2992@gmail.com>
2017-09-22 17:19:03 +0800

    Hide warnings on cronjob

--- cronjob.php
+++ cronjob.php
@@ -1,43 +1,47 @@
 <?php 
-namespace App;  
-require_once "conf.php";
 
+namespace App;
+
+require_once "conf.php";
 use App\lib\Helper;
 
-class CronJob{
-	function __construct()
-	{
-		$this->dir = __DIR__.'/uploads/';
-		$this->deleted_count = 0;
+class CronJob
+{
+    public function __construct()
+    {
+        $this->dir = __DIR__.'/uploads/';
+        $this->deleted_count = 0;
 
-		// delete from output folder
-		$this->deleteOldFiles( glob($this->dir."output/*") );
-		// delete from source folder
-		$this->deleteOldFiles( glob($this->dir."source/*") );
-	}
+        // delete from output folder
+        $this->deleteOldFiles(glob($this->dir."output/*"));
+        // delete from source folder
+        $this->deleteOldFiles(glob($this->dir."source/*"));
+    }
 
-	private function deleteOldFiles($dir){
-		foreach($dir as $key => $file) {
-			$str = "$key: ".$file;
-			// current time minus 60 seconds * 60 (minutes) * 24 (hours) * 2 (days).
-			$current_time = time() - (60 * 60 * 24 * 2);
-		    if (filemtime($file) < $current_time ) {
-		        if (is_dir($file)){
-		        	Helper::rrmdir($file);
-		        }elseif (is_file($file)){
-		        	unlink($file);
-		        }
-		        $str.= "(success: filetime: ".filemtime($file).", current_time: $current_time)";
-		        $this->deleted_count++;
-		    }else{
-		    	$str.= "(failed: filetime: ".filemtime($file).", current_time: $current_time)";
-		    }
+    private function deleteOldFiles($dir)
+    {
+        foreach ($dir as $key => $file) {
+            $str = "$key: ".$file;
+            // current time minus 60 seconds * 60 (minutes) * 24 (hours) * 2 (days).
+            $current_time = time() - (60 * 60 * 24 * 2);
+            if (@filemtime($file) < $current_time) {
+                if (is_dir($file)) {
+                    Helper::rrmdir($file);
+                } elseif (is_file($file)) {
+                    unlink($file);
+                }
+                $str.= "(success: filetime: ".@filemtime($file).", current_time: $current_time)";
+                $this->deleted_count++;
+            } else {
+                $str.= "(failed: filetime: ".@filemtime($file).", current_time: $current_time)";
+            }
 
-		    Helper::pre($str);
-		}
+            Helper::pre($str);
+        }
 
-		echo '<br/><strong>Deleted a total of <span style="color:red;">'.$this->deleted_count.'</span> file/s.</strong><br/>';
-	}
+        echo '<br/><strong>Deleted a total of <span style="color:red;">'
+            .$this->deleted_count.'</span> file/s.</strong><br/>';
+    }
 }
 
 new CronJob();
